6d2cfb22b9c02360316bb5b0a47a80f7d9fc54c6
h182032 <h182032@stud.hib.no>
2019-02-24 19:29:04 +0100

    some changes

--- project2/src/no/hvl/dat110/messages/PublishMsg.java
+++ project2/src/no/hvl/dat110/messages/PublishMsg.java
@@ -4,5 +4,28 @@ public class PublishMsg extends Message {
 	
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
+	private String msg;
+	
+	
+	public PublishMsg() {
+		
+	}
+	
+	public PublishMsg(String msg) {
+		this.msg=msg;
+	}
+	
+	public String getMessage() {
+		return msg;
+	}
+	
+	public void setMessage(String msg) {
+		this.msg = msg;
+	}
+	@Override
+	public String toString() {
+		return "Message ";
+	};
+	
 	
 }
--- project2/src/no/hvl/dat110/messages/SubscribeMsg.java
+++ project2/src/no/hvl/dat110/messages/SubscribeMsg.java
@@ -2,6 +2,12 @@ package no.hvl.dat110.messages;
 
 public class SubscribeMsg extends Message {
 
+	private String Topic;
+	
+	public SubscribeMsg(String user, String topic, String message) {
+	super(MessageType.SUBSCRIBE, user);
+	
+	}
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
 
