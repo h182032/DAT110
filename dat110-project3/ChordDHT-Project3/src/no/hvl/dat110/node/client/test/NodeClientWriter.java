14098bcd8f69c1bacb2fd8ecadb88d853bd56e1a
Ingrid <h182032@stud.hib.no>
2019-04-02 18:15:51 +0200

    implementing test

--- dat110-project3/ChordDHT-Project3/src/no/hvl/dat110/node/client/test/NodeClientWriter.java
+++ dat110-project3/ChordDHT-Project3/src/no/hvl/dat110/node/client/test/NodeClientWriter.java
@@ -1,5 +1,7 @@
 package no.hvl.dat110.node.client.test;
 
+import java.math.BigInteger;
+
 /**
  * exercise/demo purpose in dat110
  * @author tdoy
@@ -8,11 +10,8 @@ package no.hvl.dat110.node.client.test;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.Registry;
-import java.util.List;
 
 import no.hvl.dat110.file.FileManager;
-import no.hvl.dat110.node.Message;
 import no.hvl.dat110.rpc.StaticTracker;
 import no.hvl.dat110.rpc.interfaces.ChordNodeInterface;
 import no.hvl.dat110.util.Hash;
@@ -23,34 +22,47 @@ public class NodeClientWriter extends Thread {
 	private boolean succeed = false;
 	private String content;
 	private String filename;
-	
+
 	public NodeClientWriter(String content, String filename) {
 		this.content = content;
 		this.filename = filename;
 	}
-	
+
 	public void run() {
 		sendRequest();
 	}
-	
+
 	private void sendRequest() {
-		
-		// Lookup(key) - Use this class as a client that is requesting for a new file and needs the identifier and IP of the node where the file is located
-		// assume you have a list of nodes in the tracker class and select one randomly. We can use the Tracker class for this purpose
-		
-		// connect to an active chord node - can use the process defined in StaticTracker 
-		
+
+		// Lookup(key) - Use this class as a client that is requesting for a new file
+		// and needs the identifier and IP of the node where the file is located
+		// assume you have a list of nodes in the tracker class and select one randomly.
+		// We can use the Tracker class for this purpose
+
+		// connect to an active chord node - can use the process defined in
+		// StaticTracker
+		String activeNode = StaticTracker.ACTIVENODES[0];
 		// Compute the hash of the node's IP address
-		
-		// use the hash to retrieve the ChordNodeInterface remote object from the registry
-		
-		// do: FileManager fm = new FileManager(ChordNodeInterface, StaticTracker.N);
-		
-		// do: boolean succeed = fm.requestWriteToFileFromAnyActiveNode(filename, content);
-					
-		
+		BigInteger ip = Hash.hashOf(activeNode);
+		// use the hash to retrieve the ChordNodeInterface remote object from the
+		// registry
+		try {
+			ChordNodeInterface node = (ChordNodeInterface) Util.locateRegistry(activeNode).lookup(ip.toString());
+			// do: FileManager fm = new FileManager(ChordNodeInterface, StaticTracker.N);
+			FileManager fm = new FileManager(node, StaticTracker.N);
+			// do: boolean succeed = fm.requestWriteToFileFromAnyActiveNode(filename,
+			// content);
+			succeed = fm.requestWriteToFileFromAnyActiveNode(filename, content);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			succeed = false;
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+			succeed = false;
+		}
+
 	}
-	
+
 	public boolean isSucceed() {
 		return succeed;
 	}
