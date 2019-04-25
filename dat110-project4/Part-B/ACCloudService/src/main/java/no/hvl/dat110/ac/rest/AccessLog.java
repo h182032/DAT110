745f09ea68f1ac9da2c2ba6d3616f4d53f09bd73
Ingrid <h182032@stud.hib.no>
2019-04-25 13:59:00 +0200

    new folder

---
+++ dat110-project4/Part-B/ACCloudService/src/main/java/no/hvl/dat110/ac/rest/AccessLog.java
@@ -0,0 +1,49 @@
+package no.hvl.dat110.ac.rest;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.gson.Gson;
+
+public class AccessLog {
+	
+	private AtomicInteger cid;
+	protected ConcurrentHashMap<Integer, AccessEntry> log;
+	
+	public AccessLog () {
+		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
+		cid = new AtomicInteger(0);
+	}
+
+	// TODO: add an access entry for the message and return assigned id
+	public int add(String message) {
+		
+		int id = cid.getAndIncrement();
+		
+		log.put(id, new AccessEntry(id, message));
+		
+		return id;
+	}
+		
+	// TODO: retrieve a specific access entry 
+	public AccessEntry get(int id) {
+		
+		return log.get(id);
+		
+	}
+	
+	// TODO: clear the access entry log
+	public void clear() {
+		
+		log.clear();
+		
+	}
+	
+	// TODO: JSON representation of the access log
+	public String toJson () {
+    	
+		String json = new Gson().toJson(log.values().toArray());
+    	
+    	return json;
+    }
+}
